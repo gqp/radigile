1504e44c673be5e9806125a0dfe1969f436d6260
Garrick Plaisted <gqplaisted@gmail.com>
2025-03-29 16:52:47 -0700

    User Registration View - lol opps forgot the Auth\ in the path

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Auth\RegisterController;
 
 // Homepage Routes
 Route::get('/', [HomeController::class, 'index'])->name('homepage');
