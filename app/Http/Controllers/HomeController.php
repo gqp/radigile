9d12009ba6df346261e98876cb198f017f812d4e
Garrick Plaisted <gqplaisted@gmail.com>
2025-03-29 10:08:46 -0700

    Correcting the index function in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,6 @@ class HomeController extends Controller
     public function index()
     {
         // Pass any required data to the homepage view
-        return view('homepage');
+        return view('homepage_view');
     }
 }
