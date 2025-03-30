37be9806fefd89e86b4259f371511a5ff1510a54
Garrick Plaisted <gqplaisted@gmail.com>
2025-03-29 18:28:15 -0700

    adding csrf token to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Application</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
     <header>
