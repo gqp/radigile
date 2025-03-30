dd654115f5e6c58085de176cb4a0de8833c32f54
Garrick Plaisted <gqplaisted@gmail.com>
2025-03-29 20:10:55 -0700

    Lets try this

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(VerifyCsrfToken::class);
         $middleware->append(StartSession::class);
-
+        $middleware->append(VerifyCsrfToken::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
