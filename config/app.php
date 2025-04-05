0a261160205b3f9177d72bd1da9af884e9ec63fa
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-05 16:51:34 -0700

    Adding Radigile.com to config/app.php

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Radigile.com - A New Era of Team Growth'),
 
     /*
     |--------------------------------------------------------------------------
