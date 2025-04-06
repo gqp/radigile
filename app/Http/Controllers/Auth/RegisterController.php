f90b4f4a16a19664742714c50e0575ac36839773
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-05 22:21:13 -0700

    Invite Only Registration - Added invite model to resgister controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Models\Invite;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
