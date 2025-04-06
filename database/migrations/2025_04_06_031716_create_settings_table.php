9e743d03f427f548f638ed7105dc0be6a9d120a6
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-05 21:51:22 -0700

    Invite Only Registration - adjusted settings migration

--- database/migrations/2025_04_06_031716_create_settings_table.php
+++ database/migrations/2025_04_06_031716_create_settings_table.php
@@ -9,7 +9,7 @@ return new class extends Migration {
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
-            $table->string('key');
+            $table->string('name')->unique();
             $table->string('value')->nullable();
             $table->timestamps();
         });
