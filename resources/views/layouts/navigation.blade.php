634e20ae34683a280fa23e0f798046c668b1610f
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-05 23:45:14 -0700

    Fixed navigation blade

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -52,7 +52,7 @@
                             <a class="nav-link {{ request()->routeIs('admin.invites.index') ? 'active' : '' }}" href="{{ route('admin.invites.index') }}">Manage Invites</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link {{ request()->routeIs('admin.users.manage-users') ? 'active' : '' }}" href="{{ route('admin.users.manage-users') }}">Manage Users</a>
+                            <a class="nav-link {{ request()->routeIs('admin.manage-users') ? 'active' : '' }}" href="{{ route('admin.manage-users') }}">Manage Users</a>
                         </li>
                     @endif
 
