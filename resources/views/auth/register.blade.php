5bdb60be6b868975ee28c7ae7340e574e4431ed5
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-07 17:37:52 -0700

    Removing the need for nID to register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -142,22 +142,6 @@
                     </div>
                 </div>
                 <div class="row mb-3">
-                    <label for="nId"
-                           class="col-md-4 col-form-label text-md-end">{{ __('National Id') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="nId" type="text"
-                               class="form-control @error('nId') is-invalid @enderror" name="nId"
-                               value="{{ old('nId') }}" required autocomplete="name">
-
-                        @error('nId')
-                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                        @enderror
-                    </div>
-                </div>
-                <div class="row mb-3">
                     <label for="email"
                            class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
 
