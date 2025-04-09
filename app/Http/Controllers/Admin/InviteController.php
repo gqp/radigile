73747c72a6e6f63e5d1eea53e3f123bd58bafdc5
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-09 16:07:52 -0700

    Adding the ability for admins to send invites to notifyme submissions

--- app/Http/Controllers/Admin/InviteController.php
+++ app/Http/Controllers/Admin/InviteController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Mail\InviteNotification;
 use App\Models\Invite;
 use App\Models\User;
+use App\Models\NotifyMe;
 use App\Models\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
