0aa0bef677b255b390dd4bd6d2f9f525f6891de5
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-09 19:19:56 -0700

    Opps for to add something

--- app/Http/Controllers/Admin/AdminNotifyController.php
+++ app/Http/Controllers/Admin/AdminNotifyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use App\Models\NotifyMe;
 use App\Models\Setting;
 use App\Models\Invite;
