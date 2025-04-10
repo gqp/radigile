203d0126e63f5f70327c34922cde66fccb0a4c2b
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-10 15:41:20 -0700

    Refactoring the notify-me page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,15 +36,6 @@
 @include('layouts.admin.sidebar')
 <!-- Content Wrapper -->
 <div class="content-wrapper">
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">@yield('header-title', 'Dashboard')</h1>
-                </div>
-            </div>
-        </div>
-    </div>
     <!-- Main Content -->
     <div class="content">
         <div class="container-fluid">
