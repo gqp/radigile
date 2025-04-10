ca828a9bea6db97f7214ac7258a70e29cb744ba2
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-10 16:19:00 -0700

    Fixing manage users page

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Subscription;
 use App\Models\Plan;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
