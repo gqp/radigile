d2a97faee9184bff92ff0013ca1876ca653551f5
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-11 19:04:14 -0700

    refactoring routes

--- routes/web.php
+++ routes/web.php
@@ -29,11 +29,11 @@ Route::middleware(['web'])->group(function () {
 
     // Notify Me Route
     Route::post('/notify-me', [NotifyController::class, 'store'])->name('notify.store');
+
 });
 
 // Admin Routes
-Route::prefix('admin')->middleware(['web', 'auth', 'role:Admin', CheckActiveStatus::class])->group(function () {
-    // Dashboard
+Route::group(['prefix' => 'admin', 'middleware' => ['web', 'auth', 'role:Admin', CheckActiveStatus::class]], function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
     Route::get('/profile', [AdminController::class, 'profile'])->name('admin.profile');
     Route::get('/settings', [AdminController::class, 'settings'])->name('admin.settings');
@@ -63,29 +63,47 @@ Route::prefix('admin')->middleware(['web', 'auth', 'role:Admin', CheckActiveStat
     Route::get('/manage-users/create', [UserController::class, 'create'])->name('admin.users.create');
     Route::post('/manage-users', [UserController::class, 'store'])->name('admin.users.store');
     Route::patch('/admin/users/{id}/toggle-active', [UserController::class, 'toggleActive'])->name('admin.users.toggle-active');
+
+    // Edit User Routes
     Route::get('/manage-users/{user}/edit', [UserController::class, 'edit'])->name('admin.users.edit');
     Route::put('/manage-users/{user}', [UserController::class, 'update'])->name('admin.users.update');
+
+    // Delete User
     Route::delete('/manage-users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy');
 
-    // Notifications
+    // Notify Me Routes
     Route::get('/admin/notify-me', [AdminNotifyController::class, 'index'])->name('admin.notify-me');
     Route::post('/notify-me/toggle-global', [AdminNotifyController::class, 'toggleGlobal'])->name('admin.notify-me.toggle-global');
     Route::post('notify-me/{id}/send-invite', [AdminNotifyController::class, 'sendInvite'])->name('admin.notify.send-invite');
 
-    // Subscriptions and Plans
+    // Subscriptions and Plan Routes
     Route::get('/subscriptions/plans', [SubscriptionController::class, 'indexPlans'])->name('admin.plans.index');
+    Route::get('/subscriptions/plans/create', [SubscriptionController::class, 'createPlan'])->name('admin.plans.create');
+    Route::post('/subscriptions/plans', [SubscriptionController::class, 'storePlan'])->name('admin.plans.store');
+    Route::get('/subscriptions/', [SubscriptionController::class, 'indexSubscriptions'])->name('admin.subscriptions.index');
+    Route::get('/subscriptions/create', [SubscriptionController::class, 'createSubscription'])->name('admin.subscriptions.create');
+    Route::post('/subscriptions/store', [SubscriptionController::class, 'storeSubscription'])->name('admin.subscriptions.store');
+    Route::get('/subscriptions/{subscription}/edit', [SubscriptionController::class, 'editSubscription'])->name('admin.subscriptions.edit');
+    Route::put('/subscriptions/{subscription}', [SubscriptionController::class, 'updateSubscription'])->name('admin.subscriptions.update');
+    Route::get('/subscriptions/plans/{plan}/edit', [SubscriptionController::class, 'editPlan'])->name('admin.plans.edit'); // Show edit form
+    Route::put('/subscriptions/plans/{plan}', [SubscriptionController::class, 'updatePlan'])->name('admin.plans.update'); // Handle form submission
 });
 
-// Authenticated and Active User Routes
-Route::middleware(['auth', CheckActiveStatus::class])->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+// User Routes
+Route::group(['prefix' => 'user', 'middleware' => ['web', 'auth', 'verified', 'role:User', CheckActiveStatus::class]], function () {
+    Route::get('/dashboard', [UserController::class, 'index'])->name('user.dashboard');
+    Route::get('/profile', [UserController::class, 'profile'])->name('user.profile');
+    Route::put('/profile/update-name', [UserController::class, 'updateName'])->name('user.updateName');
+    Route::put('/profile/update-password', [UserController::class, 'updatePassword'])->name('user.updatePassword');
 
-    Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+    // Subscription Routes
+    Route::post('/subscribe/free', [SubscriptionController::class, 'subscribeToFreePlan'])->name('subscribe.free');
+});
 
-    Route::get('/subscriptions', [SubscriptionController::class, 'index'])->name('subscriptions.index');
-    Route::get('/subscriptions/{id}/details', [SubscriptionController::class, 'details'])->name('subscriptions.details');
+// Subscription Routes
+Route::middleware(['web', 'auth', CheckActiveStatus::class])->group(function () {
+    Route::get('/access-feature', [SubscriptionController::class, 'accessFeature'])->name('subscription.access-feature');
+    Route::get('/check-free-tier', [SubscriptionController::class, 'checkFreeTier'])->name('subscription.check-free-tier');
 });
 
 // Authentication and Email Verification Routes
