02e331194d835016959743a3ac27b070fbd31928
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-13 15:21:46 -0700

    a little more refactoring

--- app/Http/Middleware/ForcePasswordReset.php
+++ app/Http/Middleware/ForcePasswordReset.php
@@ -21,6 +21,11 @@ class ForcePasswordReset
         if ($user && $user->force_password_reset) {
             \Log::info('User requires password reset.', ['user_id' => $user->id]);
 
+            // Skip force password reset middleware for POST submission to the process route
+            if ($request->route()->getName() === 'password.force.reset.process') {
+                return $next($request); // Let the form submission process continue
+            }
+
             if ($request->route()->getName() !== 'password.force.reset') {
                 return redirect()->route('password.force.reset')
                     ->with('warning', 'You must reset your password before proceeding.');
