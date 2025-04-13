af5e3926d1460991f06890d9a6655ff01dee2045
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-13 10:52:57 -0700

    a little more refactoring

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\ForcePasswordReset;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Login;
 
@@ -21,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        parent::boot();
-
         // Register middleware aliases
         Route::aliasMiddleware('force.password.reset', ForcePasswordReset::class);
     }
