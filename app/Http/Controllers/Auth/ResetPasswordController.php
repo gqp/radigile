9f0adc98410d37590784186c2a8dc881f29c4772
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-17 15:01:02 -0700

    Forgot to import something else :(

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
 use App\Models\User;
 
 class ResetPasswordController extends Controller
