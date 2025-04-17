568261653617ced13e73c12f9a37580ff2659c80
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-17 15:04:04 -0700

    Opps

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Support\Str;
+
 use App\Models\User;
 
 class ResetPasswordController extends Controller
