91c81759bf4b7a3cfb2d51a5b1e8920566ac0d8e
Garrick Plaisted <gqplaisted@gmail.com>
2025-04-17 14:57:43 -0700

    Forgot to add Facades\Password to RestPasswordCOntroller

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 use App\Models\User;
 
 class ResetPasswordController extends Controller
